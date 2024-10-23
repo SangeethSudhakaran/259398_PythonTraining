3e854c1d265efe9e6b585415f67151c9fa269e06
Sangeeth Sudhakaran <sangeethcs2010@gmail.com>
2024-10-23 17:25:40 +0530

    Day3

---
+++ DAY04/ForLoopDemo.py
@@ -0,0 +1,11 @@
+def PrintName(name,number):
+    for i in range(1,number+1):
+        print(i,' : ', name)
+
+print("Please enter your Name!")
+name = input()
+
+print("Please enter number of times the name need to be printed")
+loopCount = int(input())
+
+PrintName(name,loopCount)
